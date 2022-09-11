568123c3b1bcdf4da8ffcfd9021bba894e2a183b
Валентин Климов <dd_apis@mail.ru>
2022-09-11 17:21:03 +0300

    update sshkey

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось!')
+    return HttpResponse('У меня получилось что-то сделать!')
 
 
 def second_page(request):
